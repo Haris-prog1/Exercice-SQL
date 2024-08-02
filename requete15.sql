ded6ac141703a3e65a6c5629849be4b8124e2db1
Haris-prog1 <harissuljagic3@gmail.com>
2024-08-02 10:03:57 +0200

    Update requete15.sql

--- requete15.sql
+++ requete15.sql
@@ -1,11 +1,10 @@
 Exercice 15
 SELECT recipe_name FROM recipe
 INNER JOIN ingredient
-WHERE price > '2'
-
 
 SELECT recipe.recipe_name FROM recipe_ingredient
 INNER JOIN ingredient
 INNER JOIN recipe
 WHERE recipe.id_recipe = recipe_ingredient.id_recipe
 GROUP BY recipe_name
+WHERE price > '2'
